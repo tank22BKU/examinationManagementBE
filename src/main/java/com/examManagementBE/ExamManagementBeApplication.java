37e6b944a6f1f07a436e1282478a84d44fdf8923
DANG VAN TAN <161315905+tank22BKU@users.noreply.github.com>
2025-10-26 15:51:20 +0700

    Initial commit

---
+++ src/main/java/com/examManagementBE/ExamManagementBeApplication.java
@@ -0,0 +1,13 @@
+package com.examManagementBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamManagementBeApplication.class, args);
+	}
+
+}
